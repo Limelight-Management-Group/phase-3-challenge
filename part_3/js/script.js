8842c63dc7df83e789c3ceb664eb8e8626a6d199
waynebanks <thestorerun@yahoo.com>
2017-08-06 12:13:09 -0700

    used regex to remove $, so I could use the .reduce() and added $ in, when I had value. Also, used a .toFixed, to limit the amount of decimals

--- part_3/js/script.js
+++ part_3/js/script.js
@@ -9,6 +9,8 @@ let clearButton = document.querySelector('.modal-clear')
 let shopCart = []
 let shopCartName = document.querySelector('#name')
 let shopCartPrice = document.querySelector('#price')
+let total = document.querySelector('.modal-section-amount')
+let priceArr = [] 
 
 addToCart.forEach((elements, index) =>
   elements.addEventListener('click', cartAdd(index))
@@ -20,8 +22,8 @@ clearButton.addEventListener('click', clearModal)
   
   function clearModal(){
   	shopCart = []
-  	console.log('this is the shopCart: ', shopCart)
   	shopCartSize = 0;
+  	console.log('this is the shopCart: ', shopCart)
   	//now show most recent cart totals!
   }
 
@@ -31,9 +33,8 @@ function cartAdd(index) {
 	  name: names[index].textContent,
 	  price: prices[index].textContent,
 	})
-
 	console.log('shopCart', shopCart)
-
+	updateTotal()
 	// alert('this just pushed, dawg!')
   }
 }
@@ -56,6 +57,29 @@ function openModal(){
 						}
 							price = shopCartPrice.innerHTML = itemprice.price
 							console.log('this is the price', price)
+							priceArr.push(price)
+							console.log('pushed', price)
+							console.log('this is price arr', priceArr)
+
+
+							let RegArr = priceArr.toString()
+							const regie = /\$/g
+
+
+							let outcome = RegArr.replace(regie, ' ').split(',').map((price)=>{
+							  let number = parseFloat(price, 10)
+							  console.log('this is number', number)
+							  return number
+							})
+							// let total = parseFloat(outcome)
+							console.log('this is outcome',outcome)
+							let subtotalval = outcome.reduce((a,b) =>{
+								// console.log(a +b )
+							  return (a+b)
+							})
+							console.log(subtotalval)
+							let setval = total.innerHTML = '$' + Number((subtotalval).toFixed(4))
+							console.log(setval)
 					})
 					// shopCart[0].price
 					let name = shopCartName.innerHTML = shopCart[0].name
@@ -74,4 +98,8 @@ function closeModal(){
 	// div.classList.remove('foo')
 	modal.classList.remove('hide-overlay')
 }	
+	 function updateTotal(){
+		// let newTotal = total.innerHTML = subtotal
+		console.log('this is working, dawg!!')
+	}
 
